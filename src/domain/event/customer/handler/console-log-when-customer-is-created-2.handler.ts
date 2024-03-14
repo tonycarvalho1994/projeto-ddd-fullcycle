8a6ffb1a08f6bb1f4e9971cedbda32b5320071ab
Tony Jedson Beserra Carvalho <tony.carvalho@br.experian.com>
2024-03-14 09:13:21 -0300

    Implementando Domain Events

--- src/domain/event/customer/handler/console-log-when-customer-is-created-2.handler.ts
+++ src/domain/event/customer/handler/console-log-when-customer-is-created-2.handler.ts
@@ -1,7 +1,7 @@
 import EventHandlerInterface from "../../@shared/event-handler.interface";
 import CustomerCreatedEvent from "../customer-created.event";
 
-export default class ConsoleLogWhenCustomerIsCreatedHandler1 implements EventHandlerInterface {
+export default class ConsoleLogWhenCustomerIsCreatedHandler1 implements EventHandlerInterface<CustomerCreatedEvent> {
     handle(event: CustomerCreatedEvent): void {
         console.log("Esse é o segundo console.log do evento: CustomerCreated")
     }
